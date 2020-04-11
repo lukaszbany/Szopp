eadb42942488814d6ea0aae0caf5465f3429f1fb
Lukasz Bany <lbany89@gmail.com>
2020-04-11 14:44:38 +0200

    initial commit

---
+++ src/main/java/pl/betweenthelines/szopp/SzoppApplication.java
@@ -0,0 +1,13 @@
+package pl.betweenthelines.szopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzoppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzoppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/betweenthelines/szopp/SzoppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.betweenthelines.szopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
